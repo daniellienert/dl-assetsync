dc0ead6be43121d5834a89af9681910adecf8016
Daniel Lienert <daniel@lienert.cc>
2020-12-18 07:11:05 +0100

    TASK: Apply migration Neos.Flow-20201109224100
    Adjust DB migrations to Doctrine Migrations 3.0

--- Migrations/Postgresql/Version20190108153502.php
+++ Migrations/Postgresql/Version20190108153502.php
@@ -1,7 +1,7 @@
 <?php
 namespace Neos\Flow\Persistence\Doctrine\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 class Version20190108153502 extends AbstractMigration
